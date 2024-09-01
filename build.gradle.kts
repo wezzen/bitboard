8f7946dcff34561b815f6114309019fcd6a7eb46
Dmitry <dimariqqq@gmail.com>
2024-09-01 18:05:26 +0600

    add errorprone and jacoco as dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,24 @@
 plugins {
-    id("java")
+    java
+    id("net.ltgt.errorprone") version "4.0.1"
+    jacoco
 }
 
 group = "com.github.wezzen.chess"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.google.errorprone:error_prone_core:2.29.2")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:5.13.0")
@@ -17,4 +26,14 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(false)
+    }
 }
