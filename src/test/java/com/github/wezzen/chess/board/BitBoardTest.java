5019d28d2464a925ba20a673595f77ae228c9e2b
Dmitry <dimariqqq@gmail.com>
2024-09-03 21:56:25 +0600

    fix: deleted wrong test for BitBoard.java

--- src/test/java/com/github/wezzen/chess/board/BitBoardTest.java
+++ src/test/java/com/github/wezzen/chess/board/BitBoardTest.java
@@ -1,20 +1,13 @@
 package com.github.wezzen.chess.board;
 
-import com.github.wezzen.chess.board.generator.BitBoardGenerator;
 import com.github.wezzen.chess.board.generator.StandardBoardGenerator;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BitBoardTest {
 
     private static final long STANDARD_BOARD_FULL_MASK = 0xFFFF00000000FFFFL;
-
-    @Test
-    void BitBoardCreatingInstance() {
-        assertDoesNotThrow(() -> new BitBoard(Mockito.mock(BitBoardGenerator.class)));
-    }
 
     @Test
     void boardTest() {
