86a6f3b3eb71ebcc52cc1f1e25c6389a4d93db86
Dmitry <dimariqqq@gmail.com>
2024-09-03 18:55:27 +0600

    refactor: refactoring constant of bitboard

--- src/main/java/com/github/wezzen/chess/board/BitBoard.java
+++ src/main/java/com/github/wezzen/chess/board/BitBoard.java
@@ -8,35 +8,51 @@ import com.github.wezzen.chess.board.generator.BitBoardGenerator;
  */
 public class BitBoard {
 
+    private static final int NUM_PLAYERS = 2;
+    
+    private static final int WHITE_INDEX = 0;
+    
+    private static final int BLACK_INDEX = 1;
+    
     /**
      * Boards for each type of figure.
      */
-    final long[] bitBoards = new long[BitBoardIndices.TOTAL];
+    final long[][] bitBoards = new long[NUM_PLAYERS][BitBoardIndices.NUM_PIECES];
+    final long boardWhite;
+    final long boardBlack;
     /**
      * Combined board with all of figure.
      */
     final long board;
 
     public BitBoard(final BitBoardGenerator generator) {
-        bitBoards[BitBoardIndices.WHITE_PAWNS] = generator.pawns(Color.WHITE);
-        bitBoards[BitBoardIndices.WHITE_KNIGHTS] = generator.knights(Color.WHITE);
-        bitBoards[BitBoardIndices.WHITE_BISHOP] = generator.bishops(Color.WHITE);
-        bitBoards[BitBoardIndices.WHITE_ROOK] = generator.rooks(Color.WHITE);
-        bitBoards[BitBoardIndices.WHITE_QUEEN] = generator.queen(Color.WHITE);
-        bitBoards[BitBoardIndices.WHITE_KING] = generator.king(Color.WHITE);
+        bitBoards[WHITE_INDEX][BitBoardIndices.PAWNS] = generator.pawns(Color.WHITE);
+        bitBoards[WHITE_INDEX][BitBoardIndices.KNIGHTS] = generator.knights(Color.WHITE);
+        bitBoards[WHITE_INDEX][BitBoardIndices.BISHOP] = generator.bishops(Color.WHITE);
+        bitBoards[WHITE_INDEX][BitBoardIndices.ROOK] = generator.rooks(Color.WHITE);
+        bitBoards[WHITE_INDEX][BitBoardIndices.QUEEN] = generator.queen(Color.WHITE);
+        bitBoards[WHITE_INDEX][BitBoardIndices.KING] = generator.king(Color.WHITE);
 
-        bitBoards[BitBoardIndices.BLACK_PAWNS] = generator.pawns(Color.BLACK);
-        bitBoards[BitBoardIndices.BLACK_KNIGHTS] = generator.knights(Color.BLACK);
-        bitBoards[BitBoardIndices.BLACK_BISHOP] = generator.bishops(Color.BLACK);
-        bitBoards[BitBoardIndices.BLACK_ROOK] = generator.rooks(Color.BLACK);
-        bitBoards[BitBoardIndices.BLACK_QUEEN] = generator.queen(Color.BLACK);
-        bitBoards[BitBoardIndices.BLACK_KING] = generator.king(Color.BLACK);
+        bitBoards[BLACK_INDEX][BitBoardIndices.PAWNS] = generator.pawns(Color.BLACK);
+        bitBoards[BLACK_INDEX][BitBoardIndices.KNIGHTS] = generator.knights(Color.BLACK);
+        bitBoards[BLACK_INDEX][BitBoardIndices.BISHOP] = generator.bishops(Color.BLACK);
+        bitBoards[BLACK_INDEX][BitBoardIndices.ROOK] = generator.rooks(Color.BLACK);
+        bitBoards[BLACK_INDEX][BitBoardIndices.QUEEN] = generator.queen(Color.BLACK);
+        bitBoards[BLACK_INDEX][BitBoardIndices.KING] = generator.king(Color.BLACK);
 
-        long b = 0L;
-        for (int i = 0; i < BitBoardIndices.TOTAL; i++) {
-            b |= bitBoards[i];
-        }
-        board = b;
+        boardWhite = packPieces(WHITE_INDEX);
+
+        boardBlack = packPieces(BLACK_INDEX);
+        
+        board = boardWhite | boardBlack;
     }
 
+    private long packPieces(final int index) {
+        long packed = 0L;
+        for (final long piece : bitBoards[index]) {
+            packed |= piece;
+        }
+        return packed;
+    }
+    
 }
--- src/main/java/com/github/wezzen/chess/board/BitBoardIndices.java
+++ src/main/java/com/github/wezzen/chess/board/BitBoardIndices.java
@@ -5,29 +5,17 @@ package com.github.wezzen.chess.board;
  */
 class BitBoardIndices {
 
-    static final int TOTAL = 12;
+    static final int NUM_PIECES = 6;
 
-    static final int WHITE_PAWNS = 0;
+    static final int PAWNS = 0;
 
-    static final int WHITE_KNIGHTS = 1;
+    static final int KNIGHTS = 1;
 
-    static final int WHITE_BISHOP = 2;
+    static final int BISHOP = 2;
 
-    static final int WHITE_ROOK = 3;
+    static final int ROOK = 3;
 
-    static final int WHITE_QUEEN = 4;
+    static final int QUEEN = 4;
 
-    static final int WHITE_KING = 5;
-
-    static final int BLACK_PAWNS = 6;
-
-    static final int BLACK_KNIGHTS = 7;
-
-    static final int BLACK_BISHOP = 8;
-
-    static final int BLACK_ROOK = 9;
-
-    static final int BLACK_QUEEN = 10;
-
-    static final int BLACK_KING = 11;
+    static final int KING = 5;
 }
